06ba222d7d605f056c0716b4b1644db69860518b
zyd16888 <zyd475182659@163.com>
2024-08-07 14:03:03 +0800

    alidrive updage

--- alidriveTV_refresh_token.py
+++ alidriveTV_refresh_token.py
@@ -1,7 +1,10 @@
+import base64
+import json
 import time
 from io import BytesIO
 
 import requests
+from Crypto.Cipher import AES
 from PIL import Image
 
 if __name__ == '__main__':
@@ -14,7 +17,7 @@ if __name__ == '__main__':
     sid = data['sid']
     # 两种登录方式都可以
     # web登录, 打开链接登录
-    print(f'点击此链接授权阿里云盘TV：https://www.aliyundrive.com/o/oauth/authorize?sid={sid}')
+    print(f'https://www.aliyundrive.com/o/oauth/authorize?sid={sid}')
     # 手机阿里云盘扫描登录
     Image.open(BytesIO(requests.get(qr_link).content)).show()
     while True:
@@ -25,8 +28,33 @@ if __name__ == '__main__':
             auth_code = status_data['authCode']
             break
     # 使用code换refresh_token
-    token_data = requests.post('http://api.extscreen.com/aliyundrive/token', data={
+    # token_data = requests.post('http://api.extscreen.com/aliyundrive/token', data={
+    #     'code': auth_code,
+    # }).json()['data']
+    # refresh_token = token_data['refresh_token']
+    # # 已有refresh_token, 直接刷新access_token
+    # token_info = requests.post('http://api.extscreen.com/aliyundrive/token', data={
+    #     'refresh_token': refresh_token,
+    # }).json()['data']
+    # 原接口404，现更新为新的加密接口
+    key = '^(i/x>>5(ebyhumz*i1wkpk^orIs^Na.'.encode()
+    # 使用code换refresh_token
+    data = requests.post('http://api.extscreen.com/aliyundrive/v2/token', data={
         'code': auth_code,
     }).json()['data']
+    plain_data = AES.new(key, AES.MODE_CBC, iv=bytes.fromhex(data['iv'])).decrypt(
+        base64.b64decode(data['ciphertext']))
+    token_data = json.loads(plain_data[:-plain_data[-1]])
     refresh_token = token_data['refresh_token']
-    print(f'refresh_token: {refresh_token}')
+    print('Refresh token:', refresh_token)
+    # 已有refresh_token, 直接刷新access_token
+    data = requests.post('http://api.extscreen.com/aliyundrive/v2/token', data={
+        'refresh_token': refresh_token,
+    }).json()['data']
+
+    print("-*-" * 20)
+    plain_data = AES.new(key, AES.MODE_CBC, iv=bytes.fromhex(data['iv'])).decrypt(
+        base64.b64decode(data['ciphertext']))
+    token_data = json.loads(plain_data[:-plain_data[-1]])
+    access_token = token_data['access_token']
+    print('Access token:', access_token)
--- alidrive_refresh_token.py
+++ alidrive_refresh_token.py
@@ -1,26 +1,39 @@
+import base64
+import json
+
 import requests
+from Crypto.Cipher import AES
 from flask import Flask
 from flask import request
 
 app = Flask(__name__)
+
+# 原接口404，现更新为新的加密接口
+key = '^(i/x>>5(ebyhumz*i1wkpk^orIs^Na.'.encode()
 
 
 @app.post('/refresh')
 def refresh():
     app.logger.info(request.json)
     refresh_token = request.json.get('refresh_token')
-    resp = requests.post('http://api.extscreen.com/aliyundrive/token', data={
+    resp = requests.post('http://api.extscreen.com/aliyundrive/v2/token', data={
         'refresh_token': refresh_token,
     })
     resp.raise_for_status()
+    print(resp.json())
     data = resp.json()['data']
     app.logger.info(data)
+    plain_data = AES.new(key, AES.MODE_CBC, iv=bytes.fromhex(data['iv'])).decrypt(
+        base64.b64decode(data['ciphertext']))
+    token_data = json.loads(plain_data[:-plain_data[-1]])
+    app.logger.info(token_data)
     return {
         'token_type': 'Bearer',
-        'access_token': data['access_token'],
-        'refresh_token': data['refresh_token'],
-        'expires_in': data['expires_in'],
+        'access_token': token_data['access_token'],
+        'refresh_token': token_data['refresh_token'],
+        'expires_in': token_data['expires_in'],
     }
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
