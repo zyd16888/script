ba29f0ba08b09326727740ca5078e9c7d69f0321
zyd16888 <zyd475182659@163.com>
2024-08-15 16:42:35 +0800

    重命名

--- file-renaming-script.py
+++ file-renaming-script.py
@@ -12,11 +12,29 @@ base_dir = "V:\\"
 
 
 def sanitize_filename(filename):
+    """
+    将文件名中的无效字符移除或替换。
+
+    参数:
+        filename (str): 需要处理的文件名。
+
+    返回:
+        str: 移除或替换无效字符后的文件名。
+    """
     # Remove or replace invalid characters
     return re.sub(r'[<>:"/\\|?*]', "", filename)
 
 
 def find_matching_document(filename):
+    """
+    查找匹配的文档。
+
+    参数:
+        filename (str): 要查找的文件名。
+
+    返回:
+        tuple: 包含两个元素的元组。第一个元素是匹配的文档（如果找到），否则为 None。第二个元素是一个布尔值，表示文件名是否包含 "-C" 后缀。
+    """
     # Remove file extension and "-C" suffix for search, but keep track of "-C"
     search_name = os.path.splitext(filename)[0]
     has_c_suffix = search_name.endswith("-C")
@@ -30,6 +48,17 @@ def find_matching_document(filename):
 
 
 def rename_files():
+    """
+    重命名文件和目录的函数。
+
+    此函数遍历指定目录中的所有文件和子目录，查找以.mp4结尾的文件（可以根据需要调整文件类型）。
+    对于每个匹配的文件，它会在MongoDB中查找匹配的文档，并根据文档中的信息生成新的文件名。
+    如果找到匹配的文档，它会重命名文件和匹配的父目录（如果父目录名称与旧文件名（不带扩展名）匹配）。
+
+    Raises:
+        Exception: 重命名文件或目录时发生的任何异常。
+
+    """
     for root, dirs, files in os.walk(base_dir):
         for filename in files:
             if filename.endswith(".mp4"):  # Adjust this if you have other file types
@@ -72,7 +101,6 @@ def rename_files():
                             print(f"Error renaming directory {old_dir_path}: {str(e)}")
                 else:
                     print(f"No matching document found for: {filename}")
-
 
 if __name__ == "__main__":
     rename_files()
